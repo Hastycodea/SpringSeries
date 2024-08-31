fd7d220f5f673485f4bc2d34d3a89eb39f951a83
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-31 08:52:33 +0300

    fasahBooks

---
+++ fasah/src/main/java/com/hastycode/fasah/FasahApplication.java
@@ -0,0 +1,13 @@
+package com.hastycode.fasah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasahApplication.class, args);
+	}
+
+}
---
+++ fasah/src/test/java/com/hastycode/fasah/FasahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.fasah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
