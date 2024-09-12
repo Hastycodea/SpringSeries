227e260180e9f607f90e222c9d2d128c5ba6bdf7
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-12 15:28:39 +0300

    fastCRUD

---
+++ FastCrud/src/test/java/com/hastycode/FastCrud/FastCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.FastCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
