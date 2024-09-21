3a963790c98b8f71abe3d9badf2e3f65a8050b6b
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-21 21:13:45 +0300

    hello

---
+++ HelloSpringProject/src/test/java/com/hastycode/HelloSpringProject/HelloSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.HelloSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
