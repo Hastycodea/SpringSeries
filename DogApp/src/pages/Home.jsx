5566b7cefeb1bfb251929d82df7d1d467125f8b2
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-11-12 18:16:24 +0300

    api

--- DogApp/src/pages/Home.jsx
+++ DogApp/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
   const [dogs, setDogs] = useState([]);
+  const [nonFiction, setNonFiction] = useState([]);
+
   useEffect(() => {
     const fetchDogData = async () => {
       try {
@@ -17,6 +20,19 @@ const Home = () => {
     fetchDogData();
   }, []);
 
+  useEffect(() => {
+    const fetchNonFiction = async () => {
+      const results = await axios.get(
+        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?api-key=${
+          import.meta.env.VITE_BOOKS_API
+        }`
+      );
+      setNonFiction(results.data.results.books);
+      // console.log(res.data.results.books);
+    };
+    fetchNonFiction();
+  }, []);
+
   return (
     <div>
       {!dogs ? (
@@ -25,12 +41,38 @@ const Home = () => {
         </h1>
       ) : (
         <section className="p-8 max-w-6xl mx-auto text-center">
-          <h1 className="flex items-center justify-center  text-3xl lg:text-5xl  text-white font-bold  " >The Dog App</h1> 
-          <p className="py-8 text-white">This website is powered by <a href="https://thedogapi.com/" className="text-indigo-600 underline active:text-orange-400">The Dog API</a></p>
+          <h1 className="flex items-center justify-center  text-3xl lg:text-5xl  text-white font-bold  ">
+            The Dog App
+          </h1>
+          <p className="py-8 text-white">
+            This website is powered by{" "}
+            <a
+              href="https://thedogapi.com/"
+              className="text-indigo-600 underline active:text-orange-400"
+            >
+              The Dog API
+            </a>
+          </p>
 
           <form action="" className="max-w-2xl mx-auto" autoComplete="off">
-            <input type="text" name="search" id="search" placeholder="Search for a dog/breed" className="w-full  py-2 px-4 rounded shadow  bg-slate-400 placeholder-white text-white outline-none"/>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search for a dog/breed"
+              className="w-full  py-2 px-4 rounded shadow  bg-slate-400 placeholder-white text-white outline-none"
+            />
           </form>
+
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+            {nonFiction.map((dog) => {
+              <article key={dog.rank} className="bg-slate-700 p-4 rounded">
+                {/* <img src={dog.image.url} alt={dog.name} className="rounded" /> */}
+                <h3>{dog.title}</h3>
+                <p>Bred for: {dog.description}</p>
+              </article>;
+            })}
+          </div>
         </section>
       )}
     </div>
