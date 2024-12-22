df68905e970f6bf13f8c780fedd0c78f08abc949
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-12-22 09:26:33 +0300

    checkmates

---
+++ checkmates/src/main/java/com/hastycode/checkmates/CheckmatesApplication.java
@@ -0,0 +1,13 @@
+package com.hastycode.checkmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmatesApplication.class, args);
+	}
+
+}
---
+++ checkmates/src/test/java/com/hastycode/checkmates/CheckmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.checkmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
