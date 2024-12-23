2a9f4b3e499ce3103881f2ed5c3cd3a9b5cbbd1c
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-12-23 22:44:29 +0300

    byId

--- ecomProject/src/main/java/com/hastycode/ecomProject/controller/ProductController.java
+++ ecomProject/src/main/java/com/hastycode/ecomProject/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.hastycode.ecomProject.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,16 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts(){
         return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+        Product product = service.getProductById(id);
+
+        if(product != null) {
+            return  new ResponseEntity<>(product, HttpStatus.FOUND);
+        } else {
+            return  new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 }
