2845b78bf46a346af6b76f2ca60063a6925517d0
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-12-24 08:44:46 +0300

    frontUps

--- ecomProject/src/main/java/com/hastycode/ecomProject/controller/ProductController.java
+++ ecomProject/src/main/java/com/hastycode/ecomProject/controller/ProductController.java
@@ -4,13 +4,11 @@ import com.hastycode.ecomProject.model.Product;
 import com.hastycode.ecomProject.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ProductController {
--- front1/ecom-frontend-1-main/src/App.jsx
+++ front1/ecom-frontend-1-main/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import AddProduct from "./components/AddProduct";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add_product" element={<AddProduct />} />   
+          <Route path="product/:id" element={<Product />} />   
+          <Route path="/cart" element={<Cart />} />   
+
         </Routes>
       </BrowserRouter>
   );
