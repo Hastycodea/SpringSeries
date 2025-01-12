427cc6703d83d0dc1d65a873ec4593ae32617cab
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-01-12 16:27:43 +0300

    jwtUpdates

--- usermanagementsystem/src/main/java/com/hastycode/usermanagementsystem/config/JWTAuthFilter.java
+++ usermanagementsystem/src/main/java/com/hastycode/usermanagementsystem/config/JWTAuthFilter.java
@@ -7,17 +7,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Configuration
+@Component
 public class JWTAuthFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -32,10 +32,21 @@ public class JWTAuthFilter extends OncePerRequestFilter {
         final String jwtToken;
         final String userEmail;
 
+        String requestPath = request.getRequestURI();
+        if(requestPath.startsWith("/auth/") || requestPath.startsWith("/public/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         if(authHeader == null || authHeader.isBlank()) {
             filterChain.doFilter(request, response);
             return;
         }
+
+//        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
 
         jwtToken = authHeader.substring(7);
         userEmail = jwtUtils.extractUsername(jwtToken);
